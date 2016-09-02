169d18086855e46ca5bf1324f99c1bfc6d95b486
王常键 <changjianwang@cyou-inc.com>
2016-09-02 11:25:51 +0800

    '删除无用文件夹'

--- build/to-cdn/js/index.bundle.js
+++
@@ -1,1 +0,0 @@
-!function(r){function t(n){if(o[n])return o[n].exports;var e=o[n]={exports:{},id:n,loaded:!1};return r[n].call(e.exports,e,e.exports,t),e.loaded=!0,e.exports}var o={};return t.m=r,t.c=o,t.p="",t(0)}([function(r,t,o){var n=o(1);n()},function(r,t){r.exports=function(){alert("Hi, mod！")}}]);
