b7373ae6cd5fef93224fc9ed6a0852e7d93133d7
王常键 <changjianwang@cyou-inc.com>
2016-09-13 14:45:47 +0800

    helpers还有问题，待解决

--- README.md
+++ README.md
@@ -7,3 +7,6 @@
     - npm install   //安装依赖
     - npm run dev   //运行开发环境  打开 http://localhost:8080
     - npm run build //编译代码  打开 http://localhost:8080/dist/ 看编译后效果
+
+
+	- jasmine-node spec/appSpec.js #跑测试，不过这里没有解决helpers问题,还要继续看看问题
--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,17 +1,17 @@
 var Jasmine = require('jasmine');
 var jasmine = new Jasmine();
 
-jasmine.loadConfigFile(process.env.JASMINE_CONFIG_PATH || './spec/support/jasmine.json');
+jasmine.loadConfigFile( './spec/support/jasmine.json');
 jasmine.loadConfig({
     spec_dir: 'spec',
     spec_files: [
-        'appSpec.js',
         '**/*[sS]pec.js'
     ],
     helpers: [
         'helpers/**/*.js'
     ],
-    "random": true
+    stopSpecOnExpectationFailure: false,
+    "random": false
 });
 
 
@@ -25,41 +25,42 @@ jasmine.loadConfig({
 //});
 jasmine.configureDefaultReporter({
     showColors: true
-    , print: function() {
-        process.stdout.write(util.format.apply(this, arguments));
-    }
+    //, print: function() {
+    //    process.stdout.write(util.format.apply(this, arguments));
+    //}
+    //,jasmineCorePath: this.jasmineCorePath
 });
 
 
-var myReporter = {
-    jasmineStarted: function(suiteInfo) {
-        console.log('Running suite with ' + suiteInfo.totalSpecsDefined);
-    },
-    suiteStarted: function(result) {
-        console.log('Suite started: ' + result.description + ' whose full description is: ' + result.fullName);
-    },
-    specStarted: function(result) {
-        console.log('Spec started: ' + result.description + ' whose full description is: ' + result.fullName);
-    },
-    specDone: function(result) {
-        console.log('Spec: ' + result.description + ' was ' + result.status);
-        for(var i = 0; i < result.failedExpectations.length; i++) {
-            console.log('Failure: ' + result.failedExpectations[i].message);
-            console.log(result.failedExpectations[i].stack);
-        }
-        console.log(result.passedExpectations.length);
-    },
-    suiteDone: function(result) {
-        console.log('Suite: ' + result.description + ' was ' + result.status);
-        for(var i = 0; i < result.failedExpectations.length; i++) {
-            console.log('AfterAll ' + result.failedExpectations[i].message);
-            console.log(result.failedExpectations[i].stack);
-        }
-    },
-    jasmineDone: function() {
-        console.log('Finished suite');
-    }
-};
+//var myReporter = {
+//    jasmineStarted: function(suiteInfo) {
+//        console.log('Running suite with ' + suiteInfo.totalSpecsDefined);
+//    },
+//    suiteStarted: function(result) {
+//        console.log('Suite started: ' + result.description + ' whose full description is: ' + result.fullName);
+//    },
+//    specStarted: function(result) {
+//        console.log('Spec started: ' + result.description + ' whose full description is: ' + result.fullName);
+//    },
+//    specDone: function(result) {
+//        console.log('Spec: ' + result.description + ' was ' + result.status);
+//        for(var i = 0; i < result.failedExpectations.length; i++) {
+//            console.log('Failure: ' + result.failedExpectations[i].message);
+//            console.log(result.failedExpectations[i].stack);
+//        }
+//        console.log(result.passedExpectations.length);
+//    },
+//    suiteDone: function(result) {
+//        console.log('Suite: ' + result.description + ' was ' + result.status);
+//        for(var i = 0; i < result.failedExpectations.length; i++) {
+//            console.log('AfterAll ' + result.failedExpectations[i].message);
+//            console.log(result.failedExpectations[i].stack);
+//        }
+//    },
+//    jasmineDone: function() {
+//        console.log('Finished suite');
+//    }
+//};
 //jasmine.addReporter(myReporter);
 
 
